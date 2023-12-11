1d598b80372394828076b5a7e2a4b40abaf237d5
MOhi <eng_mohi@ymail.com>
2023-12-11 14:19:50 +0200

    first push

---
+++ Assets/Scripts/Utilities/Extensions.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public static class Extensions
+{
+    /// <summary>
+    /// Extension method to check if a layer is in a layermask
+    /// </summary>
+    /// <param name="mask"></param>
+    /// <param name="layer"></param>
+    /// <returns></returns>
+    public static bool Contains(this LayerMask mask, int layer)
+    {
+        return mask == (mask | (1 << layer));
+    }
+
+    /// <summary>
+    /// Shuffles the element order of the specified list.
+    /// </summary>
+    public static void Shuffle<T>(this IList<T> ts)
+    {
+        var count = ts.Count;
+        var last = count - 1;
+        for (var i = 0; i < last; ++i)
+        {
+            var r = UnityEngine.Random.Range(i, count);
+            var tmp = ts[i];
+            ts[i] = ts[r];
+            ts[r] = tmp;
+        }
+    }
+
+    /// <summary>
+    /// Return position for a scrollable rect content
+    /// so for example if we want to move all the content of a scrollable rect in order to make one of it's element appear in the center
+    ///  MyScrollRect.content.localPosition = MyScrollRect.GetSnapToPositionToBringChildIntoView(someChild);
+    ///  the someChild needs to be a child to the content of the scroll rect
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="child"></param>
+    /// <returns></returns>
+    public static Vector2 GetSnapToPositionToBringChildIntoView(this ScrollRect instance, RectTransform child)
+    {
+        Canvas.ForceUpdateCanvases();
+        Vector2 viewportLocalPosition = instance.viewport.localPosition;
+        Vector2 childLocalPosition   = child.localPosition;
+        Vector2 result = new Vector2(
+                0 - (viewportLocalPosition.x + childLocalPosition.x),
+                0 - (viewportLocalPosition.y + childLocalPosition.y)
+            );
+        return result;
+    }
+
+    /// <summary>
+    /// Remap values Float
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="fromMin"></param>
+    /// <param name="fromMax"></param>
+    /// <param name="toMin"></param>
+    /// <param name="toMax"></param>
+    /// <returns></returns>
+    public static float Remap(this float from, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        var fromAbs  =  from - fromMin;
+        var fromMaxAbs = fromMax - fromMin;
+
+        var normal = fromAbs / fromMaxAbs;
+
+        var toMaxAbs = toMax - toMin;
+        var toAbs = toMaxAbs * normal;
+
+        var to = toAbs + toMin;
+
+        return to;
+    }
+
+    /// <summary>
+    /// Remap Values Int
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="fromMin"></param>
+    /// <param name="fromMax"></param>
+    /// <param name="toMin"></param>
+    /// <param name="toMax"></param>
+    /// <returns></returns>
+    public static float Remap(this int from, int fromMin, int fromMax, int toMin, int toMax)
+    {
+        float fromAbs = from - fromMin;
+        float fromMaxAbs = fromMax - fromMin;
+
+        float normal = fromAbs / fromMaxAbs;
+
+        float toMaxAbs = toMax - toMin;
+        float toAbs = toMaxAbs * normal;
+
+        float to = toAbs + toMin;
+
+        return to;
+    }
+
+}
